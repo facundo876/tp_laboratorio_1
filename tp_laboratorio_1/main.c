d1120606067c362baab90c026b14e16bd1df7c1e
facundo876 <40859125+facundo876@users.noreply.github.com>
2019-04-05 17:08:08 -0300

    Update main.c

--- tp_laboratorio_1/main.c
+++ tp_laboratorio_1/main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "funciones.h"
-/** El programa inicia con un menú en el cual se selecciona la operación deseada.
+/** El programa inicia con un menÃº en el cual se selecciona la operaciÃ³n deseada.
  * 1. Ingresar 1er operando (A=x)
  * 2. Ingresar 2do operando (B=y)
  * 3. Calcular todas las operaciones
  * 4. Informar resultados
  * 5. Salir
- * Al Seleccionar la opción de 3 se calcularan las siguientes operaciones :
+ * Al Seleccionar la opciÃ³n de 3 se calcularan las siguientes operaciones :
  * a) Calcular la suma (A+B)
  * b) Calcular la resta (A-B)
- * c) Calcular la división (A/B)
- * d) Calcular la multiplicación (A*B)
+ * c) Calcular la divisiÃ³n (A/B)
+ * d) Calcular la multiplicaciÃ³n (A*B)
  * e) Calcular el factorial (A!) y (B!)
- * La opción 4 mostrar por pantalla todos los resultados de las operaciones.
- * Con la opción 5 el programa finalizara.
+ * La opciÃ³n 4 mostrar por pantalla todos los resultados de las operaciones.
+ * Con la opciÃ³n 5 el programa finalizara.
  */
 
 int main()
@@ -22,12 +22,22 @@ int main()
     int op;
     int numeroA=0;
     int numeroB=0;
-    int sumar, restar, multiplicacion, factorialA, factorialB,verificarDivi=0;
+    int sumar, restar, multiplicacion, factorialA, factorialB;
+    int banderaNumeroA=0, banderaNumeroB=0, veriOpcion3=0;
     float division=0;
-    // Incio de el menú
+    // Incio de el menÃº
     do{
-        printf("\n1. Ingresar 1er operando (A=%d)", numeroA);
-        printf("\n2. Ingresar 2do operando (B=%d)", numeroB);
+            if(banderaNumeroA==0){
+                printf("\n1. Ingresar 1er operando (A=x)");
+            }else{
+                printf("\n1. Ingresar 1er operando (A=%d)", numeroA);
+            }
+            if(banderaNumeroB==0){
+                printf("\n2. Ingresar 2do operando (B=y)");
+            }
+            else{
+                printf("\n2. Ingresar 2do operando (B=%d)", numeroB);
+            }
         printf("\n3. Calcular todas las operaciones");
         printf("\n4. Informar resultados");
         printf("\n5. Salir\n");
@@ -36,42 +46,59 @@ int main()
 
         switch(op){
 
-            case 1:// Opción 1
+            case 1:// OpciÃ³n 1
                 printf(" Ingresar 1er operando : ");
                 scanf("%d", &numeroA);
+                 banderaNumeroA++;
                 break;
-            case 2:// Opción 2
+            case 2:// OpciÃ³n 2
                 printf(" Ingresar 2do operando : ");
                 scanf("%d", &numeroB);
+                 banderaNumeroB++;
                 break;
-            case 3:// Opción 3
+            case 3:// OpciÃ³n 3
                 sumar= suma(numeroA,numeroB);
 
                 restar= resta(numeroA,numeroB);
 
-                if(!(numeroA==0 || numeroB==0) ){
+                if(numeroB!=0){
                     division= dividir(numeroA,numeroB);
-                        verificarDivi=1;
                 }
 
                 multiplicacion= multiplicar(numeroA,numeroB);
-
                 factorialA= factoriar(numeroA);
                 factorialB= factoriar(numeroB);
-
+                    veriOpcion3=1;
                 printf("\n Operaciones completadas.\n");
                 break;
-            case 4:// Opción 4
+            case 4:// OpciÃ³n 4
+                if(veriOpcion3==0){
+                    printf("\n Es necesario calcular antes de mostrar los resultados.\n");
+                }
+                else{
                 printf("\n El resultado de A+B es: %d\n", sumar);
                 printf(" El resultado de A-B es: %d\n", restar);
-                    if(verificarDivi==0){
+                    if(numeroB==0){
                         printf(" El resultado de A/B es: Error no se puede dividir por cero!\n");
                     }
                     else{
                         printf(" El resultado de A/B es: %.2f\n", division);
                     }
                 printf(" El resultado de A*B es: %d\n", multiplicacion);
-                printf(" El factorial de A es: %d y El factorial de B es: %d\n", factorialA, factorialB);
+                if(numeroA<0){
+                    printf("El factorial de A es : Error! ");
+                }
+                else{
+                    printf(" El factorial de A es : %d ", factorialA);
+                }
+                if(numeroB<0){
+                    printf(" El factorial de B es : Error! ");
+                }
+                else{
+                    printf("El factorial de B es : %d ", factorialB);
+                }
+                printf("\n\n");
+                }
                 break;
             case 5://Opcion 5
                 printf("\n Fin.\n");
@@ -80,7 +107,7 @@ int main()
                 printf("\n Error ese operador es invalido\n");
         }
 
-    }while(op!=5);// Opción 5
-    // Fin del menú
+    }while(op!=5);// OpciÃ³n 5
+    // Fin del menÃº
     return 0;
 }
